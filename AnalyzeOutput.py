3ccb7a5c1863eaa73dab4c63171c57af598a27eb
Garrett Wilson <g@floft.net>
2016-05-20 09:10:17 -0700

    Initial analysis of output file
    Output example file and calculate some metrics based on it

---
+++ AnalyzeOutput.py
@@ -0,0 +1,44 @@
+#
+# Analyze the data from the simulation
+#
+# Output from simulation file format:
+#    PID, t submitted, t started, t completed, \
+#    total t spent in queues, total t executing, \
+#    total t waiting for I/O
+#
+# Analysis consists of:
+#    Average turnaround (T_t)
+#    Average wait in queues (W_t)
+#    Average response (R_t)
+#    Average throughput (Th)
+#
+import random
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import andrews_curves
+
+# Make them look prettier
+plt.style.use('ggplot')
+sns.set(style="ticks")
+
+# For reproducibility
+random.seed(0)
+np.random.seed(0)
+
+# Plot all the data
+df = pd.read_csv('processes/example_output.txt')
+
+turnaround = df['Completed'] - df['Submitted']
+wait = df['Queues']
+response = df['Started'] - df['Submitted']
+avgTurnaround = turnaround.mean()
+avgWait = wait.mean()
+avgResponse = response.mean()
+throughput = df['Completed'].count() / df['Completed'].iloc[-1]
+
+print("AvgTurnaround:", avgTurnaround)
+print("AvgWait:", avgWait)
+print("AvgResponse:", avgResponse)
+print("Throughput:", throughput)
--- GenerateFiles.py
+++ GenerateFiles.py
@@ -8,8 +8,9 @@ import numpy as np
 np.random.seed(0)
 
 if __name__ == "__main__":
-    #f = open(filename, 'w')
-    #f.write(line)
+    #
+    # Generate process files for running through the simulation
+    #
 
     # Parameters for each file
     maxFiles = 10
@@ -22,10 +23,9 @@ if __name__ == "__main__":
     for fn in range(0,maxFiles):
         with open(directory+"/" + str(fn) + ".txt", "w") as f:
             # Initialize for each file
-            pid = 0
             arrival = 0
 
-            for i in range(0, count):
+            for pid in range(0, count):
                 # How many times we have, starting with CPU and then alternating
                 # between CPU and IO
                 switches = np.random.randint(1,maxSwitches)
@@ -35,13 +35,46 @@ if __name__ == "__main__":
                 arrival += np.random.randint(1,maxArrivalInc)
 
                 # Create the proccess line
-                processFile = []
-                processFile.append(pid)
-                processFile.append(arrival)
+                l = []
+                l.append(pid)
+                l.append(arrival)
 
                 for j in range(0,switches):
-                    processFile.append(np.random.randint(1,maxTime))
+                    l.append(np.random.randint(1,maxTime))
 
-                pid += 1
+                f.write(",".join(str(x) for x in l) + "\r\n")
 
-                f.write(",".join(str(x) for x in processFile) + "\r\n")
+    #
+    # Generate example output file for initial plotting
+    #
+    with open(directory+"/example_output.txt", "w") as f:
+        # Initialize for each file
+        arrival = 0
+
+        # Some maximum values for random number generation
+        maxStarted = 100
+        maxCompleted = 1000
+        maxWaitQueues = 50
+        maxExecuting = 500
+        maxWaitIO = 10
+
+        f.write("PID,Submitted,Started,Completed,Queues,Executing,IO\r\n")
+
+        for pid in range(0, count):
+            arrival += np.random.randint(1,maxArrivalInc)
+            started = arrival + np.random.randint(1,maxStarted)
+            completed = arrival + np.random.randint(1,maxCompleted)
+            waitingQueues = np.random.randint(1,maxWaitQueues)
+            executing = np.random.randint(1,maxExecuting)
+            waitingIO = np.random.randint(1,maxWaitIO)
+
+            # Create the proccess line
+            l = []
+            l.append(pid)
+            l.append(arrival)
+            l.append(started)
+            l.append(completed)
+            l.append(waitingQueues)
+            l.append(executing)
+            l.append(waitingIO)
+            f.write(",".join(str(x) for x in l) + "\r\n")

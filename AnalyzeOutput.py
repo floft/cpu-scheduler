6a627b8e0352344d8f81ee4014f51e1b3482e6c0
Garrett Wilson <g@floft.net>
2016-05-23 10:57:55 -0700

    Nice plots with Seaborn from simulation data

--- AnalyzeOutput.py
+++ AnalyzeOutput.py
@@ -12,7 +12,10 @@
 #    Average response (R_t)
 #    Average throughput (Th)
 #
+import re
+import os
 import random
+import collections
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -21,24 +24,66 @@ from pandas.tools.plotting import andrews_curves
 
 # Make them look prettier
 plt.style.use('ggplot')
-sns.set(style="ticks")
+#sns.set(style="ticks")
+sns.set_style("whitegrid")
 
 # For reproducibility
 random.seed(0)
 np.random.seed(0)
 
+# Evaluate the metrics on the desired files and create a DataFrame so that we
+# can easily plot the data
+def processFiles(files):
+    results = pd.DataFrame()
+
+    for i, [fn, cores] in enumerate(files):
+        df = pd.read_csv(os.path.join(directory,fn))
+
+        turnaround = df['Completed'] - df['Submitted']
+        wait = df['Queues']
+        response = df['Started'] - df['Submitted']
+        avgTurnaround = turnaround.mean()
+        avgWait = wait.mean()
+        avgResponse = response.mean()
+        throughput = df['Completed'].count() / df['Completed'].iloc[-1]
+
+        results = results.append(pd.DataFrame([[cores, avgTurnaround, avgWait,
+            avgResponse, throughput]], index=[i],
+            columns=['Cores','AvgTurnaround','AvgWait','AvgResponse','Throughput']))
+
+    return results
+
 # Plot all the data
-df = pd.read_csv('results/0.csv')
+directory = "results"
+files = os.listdir(directory)
 
-turnaround = df['Completed'] - df['Submitted']
-wait = df['Queues']
-response = df['Started'] - df['Submitted']
-avgTurnaround = turnaround.mean()
-avgWait = wait.mean()
-avgResponse = response.mean()
-throughput = df['Completed'].count() / df['Completed'].iloc[-1]
+# Match the different tests we're doing
+re_fcfs = re.compile(".*_fcfs_cpu(.*)\.csv")
+fcfs_files = []
+for f in files:
+    m = re_fcfs.match(f)
 
-print("AvgTurnaround:", avgTurnaround)
-print("AvgWait:", avgWait)
-print("AvgResponse:", avgResponse)
-print("Throughput:", throughput)
+    if m and len(m.groups()):
+        # filename, # of cores
+        fcfs_files.append((f, int(m.groups()[0])))
+
+# FCFS with different numbers of cores
+fcfs = processFiles(fcfs_files)
+
+plt.figure()
+sns.violinplot(x="Cores", y="AvgTurnaround", data=fcfs)
+plt.title("Average Turnaround with FCFS varying number of cores")
+
+plt.figure()
+sns.violinplot(x="Cores", y="AvgWait", data=fcfs)
+plt.title("Average Wait with FCFS varying number of cores")
+
+plt.figure()
+sns.violinplot(x="Cores", y="AvgResponse", data=fcfs)
+plt.title("Average Response with FCFS varying number of cores")
+
+plt.figure()
+sns.violinplot(x="Cores", y="Throughput", data=fcfs)
+plt.title("Average Throughput with FCFS varying number of cores")
+
+plt.show()
